d4e09362cf881258cb0a26f22ca046a82ed83d4e
Gabriel Santos <gab.tec97@gmail.com>
2022-09-10 18:48:29 -0300

    Criacao da primeira versao das models Marca e Produto

--- core/models.py
+++ core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Marca(models.Model):
+    nome = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.nome
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=128)
+    peso = models.IntegerField()
+    preco = models.FloatField()
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE, related_name="marca")
+    tipo = models.CharField(max_length=64, default='Cozinha')
+
+    def __str__(self):
+        if self.tipo == 'Cozinha':
+            return f"{self.nome} | {self.marca} - {self.peso}Kg: R$ {self.preco}"
+        else:
+            return f"{self.nome} | {self.marca} - {self.peso} uni: R$ {self.preco}"
